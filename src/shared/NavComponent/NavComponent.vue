389620c97ccced285a2fe1f8715fb49d234dad34
Alejandro <alesf22@gmail.com>
2021-12-22 03:56:49 +0100

    created nav layout

---
+++ src/shared/NavComponent/NavComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="nav-container">
+        <ul>
+            <li data-content="profile" ><i class="fas fa-user-circle"></i></li>
+            <li data-content="chat" ><i class="far fa-comment-dots"></i></li>
+            <li data-content="logout" ><i class="fal fa-sign-out-alt"></i></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavComponent'
+}
+</script>
+
+<style lang="scss" src="./NavComponent.scss" ></style>
--- src/views/Chat/Chat.vue
+++ src/views/Chat/Chat.vue
@@ -1,17 +1,20 @@
 <template>
-    <ChatComponent style="display: none"></ChatComponent>
-    <ProfileComponent></ProfileComponent>
+    <NavComponent></NavComponent>
+    <ChatComponent ></ChatComponent>
+    <ProfileComponent style="display: none"></ProfileComponent>
 </template>
 
 <script>
 import ChatComponent from '../../components/ChatComponent/ChatComponent.vue';
 import ProfileComponent from '../../components/ProfileComponent/ProfileComponent.vue';
+import NavComponent from '../../shared/NavComponent/NavComponent.vue';
 
 export default {
     name: 'Chat',
     components:{
         ChatComponent,
         ProfileComponent,
+        NavComponent,
     }
 }
 </script>
