5c7470a4146ff77c1eb7d4b142c9d0b6380bcdf1
Alejandro <alesf22@gmail.com>
2021-12-22 01:34:42 +0100

    created profile layout

---
+++ src/components/ProfileComponent/ProfileComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="profile-container">
+        <span class="blur-background"></span>
+        <div class="profile-wrapper">
+            <h4>Your profile:</h4>
+            <table>
+                <tr class="photo-row">
+                    <td class="first-column">Photo:</td>
+                    <td><img  src="https://cdn-icons.flaticon.com/png/512/552/premium/552909.png?token=exp=1640122029~hmac=b9acffd82dda71cb658daab06220261f"  alt=""></td>
+                </tr>
+                <tr>
+                    <td class="first-column">Name:</td>
+                    <td>Alejandro Sandí</td>
+                </tr>
+                <tr>
+                    <td class="first-column" >Email:</td>
+                    <td>alesf22@gmail.com</td>
+                </tr>
+                <tr>
+                    <td class="first-column">Password:</td>
+                    <td>*********</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><button class="edit-button">Edit</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileComponent'
+}
+</script>
+
+<style lang="scss" src="./ProfileComponent.scss"></style>
--- src/views/Chat/Chat.vue
+++ src/views/Chat/Chat.vue
@@ -1,14 +1,17 @@
 <template>
-    <ChatComponent></ChatComponent>
+    <ChatComponent style="display: none"></ChatComponent>
+    <ProfileComponent></ProfileComponent>
 </template>
 
 <script>
-import ChatComponent from '../../components/ChatComponent/ChatComponent.vue'
+import ChatComponent from '../../components/ChatComponent/ChatComponent.vue';
+import ProfileComponent from '../../components/ProfileComponent/ProfileComponent.vue';
 
 export default {
     name: 'Chat',
     components:{
         ChatComponent,
+        ProfileComponent,
     }
 }
 </script>
